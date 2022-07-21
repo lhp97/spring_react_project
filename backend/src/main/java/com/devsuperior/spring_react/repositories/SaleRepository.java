b6950e6a16baa61de962012cbfd7535416bc8c6c
lhp97 <lhdprado@gmail.com>
2022-07-20 21:40:30 -0300

    Date select

--- backend/src/main/java/com/devsuperior/spring_react/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/spring_react/repositories/SaleRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.spring_react.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.spring_react.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Integer>{
+	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :dateMin AND :dateMax ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate dateMin, LocalDate dateMax, Pageable pageable);
 
 }
--- backend/src/main/java/com/devsuperior/spring_react/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/spring_react/service/SaleService.java
@@ -1,12 +1,19 @@
 package com.devsuperior.spring_react.service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.spring_react.entities.Sale;
 import com.devsuperior.spring_react.repositories.SaleRepository;
+
+import lombok.val;
 
 @Service
 public class SaleService {
@@ -14,8 +21,25 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
-	public List<Sale> findSales() {
-		return saleRepository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate localDateMin = minDate.isEmpty() ? getDateAYearAgo() : convertTextDate(minDate);
+		LocalDate localDateMax = maxDate.isEmpty() ? getTodayDate() : convertTextDate(maxDate);
+		
+		return saleRepository.findSales(localDateMin, localDateMax, pageable);
+	}
+	
+	private LocalDate getTodayDate() {
+		return LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+	}
+	
+	private LocalDate getDateAYearAgo() {
+		return getTodayDate().minusDays(365);
+	}
+
+	
+	private LocalDate convertTextDate(String date) {
+		return LocalDate.parse(date);
 	}
 
 }
